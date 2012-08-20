3187c342e19efd69d7540624072ed3a09b6c0930
atztogo <atz.togo@gmail.com>
2012-08-20 09:08:46 +0900

    Update niggli.*.

--- src/niggli.c
+++ src/niggli.c
@@ -1,1 +1,22 @@
 /* niggli.c */
+
+typedef struct {
+  int ndim;
+  int *shape;
+} 
+
+static int step1(int C[9])
+{
+  int i, j;
+
+  for (i = 0; i < 9; i++) {
+    C[i] = 0;
+  }
+}
+		 
+
+void reduce_lattice(double lattice[3][3],
+		    const double symprec)
+{
+  
+}
---
+++ src/niggli.h
@@ -0,0 +1,3 @@
+/* niggli.h */
+void reduce_lattice(double lattice[3][3],
+		    const double symprec);
